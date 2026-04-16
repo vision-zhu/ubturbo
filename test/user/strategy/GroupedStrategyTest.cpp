695c87aacd5c587dd0c2452e277ff6a8e0c25c9f
woquzhao12138 <1825369014@qq.com>
2026-04-16 19:41:33 +0800

    smap: add basic grouped strategy unit coverage for demotion, promotion, and accounting updates.

---
+++ test/user/strategy/GroupedStrategyTest.cpp
@@ -0,0 +1,124 @@
+/*
+ * Copyright (c) Huawei Technologies Co., Ltd. 2025. All rights reserved.
+ * Description: smap grouped migration strategy unit tests
+ */
+
+#include <cstdlib>
+#include <gtest/gtest.h>
+#include "mockcpp/mokc.h"
+
+#include "manage/manage.h"
+#include "strategy/grouped_strategy.h"
+
+class GroupedStrategyTest : public ::testing::Test {
+protected:
+    void TearDown() override
+    {
+        GlobalMockObject::verify();
+    }
+};
+
+static void FreeMigList(struct MigList mlist[MAX_NODES][MAX_NODES])
+{
+    for (int i = 0; i < MAX_NODES; i++) {
+        for (int j = 0; j < MAX_NODES; j++) {
+            free(mlist[i][j].addr);
+            mlist[i][j].addr = nullptr;
+        }
+    }
+}
+
+TEST_F(GroupedStrategyTest, TestGroupedStrategyDemoteByLocalLimitAndQuota)
+{
+    ProcessAttr process = {};
+    struct MigList mlist[MAX_NODES][MAX_NODES] = {};
+    ActcData localPages[3] = {};
+
+    process.pid = 100;
+    process.groupPolicy.enabled = true;
+    process.groupPolicy.groupCount = 1;
+    process.groupPolicy.groups[0].localCount = 1;
+    process.groupPolicy.groups[0].localNids[0] = 0;
+    process.groupPolicy.groups[0].targetCount = 1;
+    process.groupPolicy.groups[0].targets[0].nid = 4;
+    process.groupPolicy.groups[0].targets[0].quotaPages = 10;
+    process.groupPolicy.groups[0].localLimitPages = 1;
+
+    localPages[0].addr = 0x3000;
+    localPages[0].freq = 3;
+    localPages[1].addr = 0x1000;
+    localPages[1].freq = 1;
+    localPages[2].addr = 0x2000;
+    localPages[2].freq = 2;
+    process.scanAttr.actcData[0] = localPages;
+    process.scanAttr.actcLen[0] = 3;
+
+    MOCKER(GetNrFreeHugePagesByNode).stubs().will(returnValue((uint64_t)10));
+
+    int ret = GroupedMigrationStrategy(&process, mlist);
+    EXPECT_EQ(0, ret);
+    EXPECT_EQ(2, mlist[0][4].nr);
+    EXPECT_EQ(0x1000, mlist[0][4].addr[0]);
+    EXPECT_EQ(0x2000, mlist[0][4].addr[1]);
+
+    FreeMigList(mlist);
+}
+
+TEST_F(GroupedStrategyTest, TestGroupedStrategyPromoteByLocalDeficit)
+{
+    ProcessAttr process = {};
+    struct MigList mlist[MAX_NODES][MAX_NODES] = {};
+    ActcData localPages[1] = {};
+    ActcData remotePages[2] = {};
+
+    process.pid = 101;
+    process.groupPolicy.enabled = true;
+    process.groupPolicy.groupCount = 1;
+    process.groupPolicy.groups[0].localCount = 1;
+    process.groupPolicy.groups[0].localNids[0] = 0;
+    process.groupPolicy.groups[0].targetCount = 1;
+    process.groupPolicy.groups[0].targets[0].nid = 4;
+    process.groupPolicy.groups[0].targets[0].quotaPages = 10;
+    process.groupPolicy.groups[0].targets[0].usedPages = 2;
+    process.groupPolicy.groups[0].localLimitPages = 3;
+
+    localPages[0].addr = 0x1000;
+    process.scanAttr.actcData[0] = localPages;
+    process.scanAttr.actcLen[0] = 1;
+    remotePages[0].addr = 0x4000;
+    remotePages[0].freq = 4;
+    remotePages[1].addr = 0x5000;
+    remotePages[1].freq = 5;
+    process.scanAttr.actcData[4] = remotePages;
+    process.scanAttr.actcLen[4] = 2;
+
+    MOCKER(GetNrFreeHugePagesByNode).stubs().will(returnValue((uint64_t)10));
+
+    int ret = GroupedMigrationStrategy(&process, mlist);
+    EXPECT_EQ(0, ret);
+    EXPECT_EQ(2, mlist[4][0].nr);
+    EXPECT_EQ(0x5000, mlist[4][0].addr[0]);
+    EXPECT_EQ(0x4000, mlist[4][0].addr[1]);
+
+    FreeMigList(mlist);
+}
+
+TEST_F(GroupedStrategyTest, TestUpdateGroupedMigrationResult)
+{
+    ProcessAttr process = {};
+
+    process.pid = 102;
+    process.groupPolicy.enabled = true;
+    process.groupPolicy.groupCount = 1;
+    process.groupPolicy.groups[0].localCount = 1;
+    process.groupPolicy.groups[0].localNids[0] = 0;
+    process.groupPolicy.groups[0].targetCount = 1;
+    process.groupPolicy.groups[0].targets[0].nid = 4;
+    process.groupPolicy.groups[0].targets[0].quotaPages = 10;
+
+    UpdateGroupedMigrationResult(&process, 0, 4, 3);
+    EXPECT_EQ(3, process.groupPolicy.groups[0].targets[0].usedPages);
+
+    UpdateGroupedMigrationResult(&process, 4, 0, 2);
+    EXPECT_EQ(1, process.groupPolicy.groups[0].targets[0].usedPages);
+}
