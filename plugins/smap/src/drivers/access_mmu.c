efbed119e9d4177b9534d020db15ab097c795bf1
dszhu <2549951418@qq.com>
2026-04-28 20:27:39 +0800

    bitmap与最后一次扫描合并
    debug

--- plugins/smap/src/drivers/access_mmu.c
+++ plugins/smap/src/drivers/access_mmu.c
@@ -94,7 +94,7 @@ static int set_non_anon_bm(struct access_pid *ap, u64 acidx, u64 paddr, int nid)
 	return 0;
 }
 
-static int add_to_bm_page(u64 paddr, struct access_pid *ap)
+int add_to_bm_page(u64 paddr, struct access_pid *ap)
 {
 	int nid, nid_pos, ret;
 	u64 acidx;
@@ -173,7 +173,7 @@ static void set_pa_prior(struct access_pid *ap, u64 vaddr, u64 pa_idx, int nid)
 	ap->info.mapping[va_idx] = map;
 }
 
-static int add_to_bm_hugepage(u64 vaddr, u64 paddr, struct access_pid *ap)
+int add_to_bm_hugepage(u64 vaddr, u64 paddr, struct access_pid *ap)
 {
 	int nid, nid_pos, ret;
 	u64 acidx;
@@ -194,17 +194,6 @@ static int add_to_bm_hugepage(u64 vaddr, u64 paddr, struct access_pid *ap)
 	set_pa_prior(ap, vaddr, acidx, nid);
 	ap->page_num[nid]++;
 	return 0;
-}
-
-static inline void add_to_bm_huge(u64 vaddr, u64 paddr, struct access_pid *ap)
-{
-	u64 mask_paddr = paddr & TWO_MEGA_MASK;
-	add_to_bm_hugepage(vaddr, mask_paddr, ap);
-}
-
-static void add_to_bm_normal(u64 paddr, struct access_pid *ap)
-{
-	add_to_bm_page(paddr, ap);
 }
 
 static bool is_paddr_belong_remote_node(u64 pa, int nid)
--- plugins/smap/src/drivers/access_mmu.h
+++ plugins/smap/src/drivers/access_mmu.h
@@ -8,10 +8,25 @@
 #define _SRC_ACCESS_MMU_H
 
 #include "access_pid.h"
+#include "check.h"
 
 extern struct list_head remote_ram_list;
 extern rwlock_t rem_ram_list_lock;
 
 void walk_pid_pagemap(struct pagemapread *pm);
 struct mm_struct *get_mm_by_pid(pid_t pid);
+
+int add_to_bm_hugepage(u64 vaddr, u64 paddr, struct access_pid *ap);
+int add_to_bm_page(u64 paddr, struct access_pid *ap);
+
+static inline void add_to_bm_huge(u64 vaddr, u64 paddr, struct access_pid *ap)
+{
+	u64 mask_paddr = paddr & TWO_MEGA_MASK;
+	add_to_bm_hugepage(vaddr, mask_paddr, ap);
+}
+
+static inline void add_to_bm_normal(u64 paddr, struct access_pid *ap)
+{
+	add_to_bm_page(paddr, ap);
+}
 #endif /* _SRC_ACCESS_MMU_H */
